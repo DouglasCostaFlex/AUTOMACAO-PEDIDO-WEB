72695adfaa1a9b6952a6f59b6ec5d568bd80c1fd
DouglasCostaFlex <130561314+DouglasCostaFlex@users.noreply.github.com>
2023-08-01 08:50:57 -0300

    commit-pasta-127

--- steps/14-FecharBancoDeDados_test.js
+++ steps/14-FecharBancoDeDados_test.js
@@ -1,29 +1,33 @@
-// Feature('EncerrarBanco').tag('EncerrarBanco');
-// const { Client } = require('pg');
-// const client = new Client({
-//     user: 'flextotal',
-//     host: '192.168.1.3',
-//     database: 'rezzadori_automacao',
-//     password: 'Fl3xt0t@L',
-//     port: 5432,
-// });
+Feature('EncerrarBanco').tag('EncerrarBanco');
+const { AbrirAllure } = require("../pages/AbrirAllure");
+const { Client } = require('pg');
+const client = new Client({
+    user: 'flextotal',
+    host: '192.168.1.3',
+    database: 'rezzadori_automacao',
+    password: 'Fl3xt0t@L',
+    port: 5432,
+});
 
-// Scenario('Encerrar conexão com o banco ', async () => {
+Scenario('Encerrar conexão com o banco ', async () => {
 
 
-//     //FECHO A CONEXAO COM O BANCO DE DADOS
-//     tryTo(() => {
-//         client.end()
-//             .then(() => {
-//                 console.log('Conexão encerrada.');
-//             })
-//             .catch((err) => {
-//                 console.error('Erro ao fechar a conexão com o banco de dados:', err);
-//             });
-//     });
+    //FECHO A CONEXAO COM O BANCO DE DADOS
+    tryTo(() => {
+        client.end()
+            .then(() => {
+                console.log('Conexão encerrada.');
+            })
+            .catch((err) => {
+                console.error('Erro ao fechar a conexão com o banco de dados:', err);
+            });
+    });
 
-//     //ENCERRO O PROGRAMA.
-//     await process.exit(0);
+    //ABRO O REPORTER
+    AbrirAllure();
 
-// })
+    //ENCERRO O PROGRAMA.
+    await process.exit(0);
+
+})
 
