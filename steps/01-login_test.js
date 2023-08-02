62e6eee766a3ddf7e3dd8d47f8e57f09e0e54846
DouglasCostaFlex <130561314+DouglasCostaFlex@users.noreply.github.com>
2023-08-02 13:42:59 -0300

    tirando comentarios- pasta 127

--- steps/01-login_test.js
+++ steps/01-login_test.js
@@ -1,237 +1,237 @@
 
-// const { Login } = require("../pages/login_page");
+const { Login } = require("../pages/login_page");
 
-// const { Pagina_Login_Wildfly1,
-//   Pagina_Produto_16410_Wildfly1,
-//   Pagina_Historico_Pedido_Wildfly1 ,
-//   Pagina_Historico_Financeiro_Wildfly1,
-//   Pagina_Login_Wildfly2 = '',
-//   Pagina_Historico_Pedido_Wildfly2 = '',
-//   Pagina_Historico_Financeiro_Wildfly2 = '',
-//   Pagina_Produto_08070_Wildfly2 = '',
-//   Pagina_Login_Wildfly3 = '',
-//   Pagina_Historico_Pedido_Wildfly3 = '',
-//   Pagina_Historico_Financeiro_Wildfly3 = '',
-//   Pagina_Produto_08070_Wildfly3 = '',
-// } = require("../pages/LinksExternos");
+const { Pagina_Login_Wildfly1,
+  Pagina_Produto_16410_Wildfly1,
+  Pagina_Historico_Pedido_Wildfly1 ,
+  Pagina_Historico_Financeiro_Wildfly1,
+  Pagina_Login_Wildfly2 = '',
+  Pagina_Historico_Pedido_Wildfly2 = '',
+  Pagina_Historico_Financeiro_Wildfly2 = '',
+  Pagina_Produto_08070_Wildfly2 = '',
+  Pagina_Login_Wildfly3 = '',
+  Pagina_Historico_Pedido_Wildfly3 = '',
+  Pagina_Historico_Financeiro_Wildfly3 = '',
+  Pagina_Produto_08070_Wildfly3 = '',
+} = require("../pages/LinksExternos");
 
-// Feature('login').tag('Login');
+Feature('login').tag('Login');
 
-// const { I } = inject()
+const { I } = inject()
 
-// const CNPJ_CORRETO = '00409260000115'
-// const SENHA_CORRETA = '48303523'
-// const CNPJ_INVALIDO = '000000000000'
-// const SENHA_INVALIDA = '000000'
-// const Telefone = '48000000000'
-// const Email = 'teste.teste@gmail.com'
-// const Razao_Social = 'RazaoTeste'
-// const Nome_Fantasia = 'FantasiaTeste'
+const CNPJ_CORRETO = '00409260000115'
+const SENHA_CORRETA = '48303523'
+const CNPJ_INVALIDO = '000000000000'
+const SENHA_INVALIDA = '000000'
+const Telefone = '48000000000'
+const Email = 'teste.teste@gmail.com'
+const Razao_Social = 'RazaoTeste'
+const Nome_Fantasia = 'FantasiaTeste'
 
-// const Nome_Contato_Financeiro = '/html/body/my-app/cadastro-novo-cliente-component/div/div/div/form/div[1]/div[4]/div[2]/mat-form-field[1]/div/div[1]/div/input'
-// const Email_Financeiro = '/html/body/my-app/cadastro-novo-cliente-component/div/div/div/form/div[1]/div[4]/div[2]/mat-form-field[2]/div/div[1]/div/input'
-// const Numero_Financeiro = '/html/body/my-app/cadastro-novo-cliente-component/div/div/div/form/div[1]/div[4]/div[2]/mat-form-field[3]/div/div[1]/div/input'
+const Nome_Contato_Financeiro = '/html/body/my-app/cadastro-novo-cliente-component/div/div/div/form/div[1]/div[4]/div[2]/mat-form-field[1]/div/div[1]/div/input'
+const Email_Financeiro = '/html/body/my-app/cadastro-novo-cliente-component/div/div/div/form/div[1]/div[4]/div[2]/mat-form-field[2]/div/div[1]/div/input'
+const Numero_Financeiro = '/html/body/my-app/cadastro-novo-cliente-component/div/div/div/form/div[1]/div[4]/div[2]/mat-form-field[3]/div/div[1]/div/input'
 
 
-// const Email_Compras = '/html/body/my-app/cadastro-novo-cliente-component/div/div/div/form/div[1]/div[4]/div[4]/mat-form-field[2]/div/div[1]/div/input'
-// const Nome_Contato_Compras = '/html/body/my-app/cadastro-novo-cliente-component/div/div/div/form/div[1]/div[4]/div[4]/mat-form-field[1]/div/div[1]/div/input'
-// const Numero_compras = '/html/body/my-app/cadastro-novo-cliente-component/div/div/div/form/div[1]/div[4]/div[4]/mat-form-field[3]/div/div[1]/div/input'
+const Email_Compras = '/html/body/my-app/cadastro-novo-cliente-component/div/div/div/form/div[1]/div[4]/div[4]/mat-form-field[2]/div/div[1]/div/input'
+const Nome_Contato_Compras = '/html/body/my-app/cadastro-novo-cliente-component/div/div/div/form/div[1]/div[4]/div[4]/mat-form-field[1]/div/div[1]/div/input'
+const Numero_compras = '/html/body/my-app/cadastro-novo-cliente-component/div/div/div/form/div[1]/div[4]/div[4]/mat-form-field[3]/div/div[1]/div/input'
 
 
-// Before(() => {
+Before(() => {
 
-//   //VOU PARA PAGINA "LOGIN"
-//   I.amOnPage(Pagina_Login_Wildfly1)
-//   //CASO DE PROBLEMA DE SEGURANÇA, APERTA EM MODO AVANÇADO E COTNINUA
-//   tryTo(() => I.waitForText('Avançado'))
-//   tryTo(() => I.click('Avançado'))
-//   tryTo(() => I.click('Ir para 192.168.1.236 (não seguro)'))
-//   I.wait(5)
+  //VOU PARA PAGINA "LOGIN"
+  I.amOnPage(Pagina_Login_Wildfly1)
+  //CASO DE PROBLEMA DE SEGURANÇA, APERTA EM MODO AVANÇADO E COTNINUA
+  tryTo(() => I.waitForText('Avançado'))
+  tryTo(() => I.click('Avançado'))
+  tryTo(() => I.click('Ir para 192.168.1.236 (não seguro)'))
+  I.wait(5)
 
-// })
+})
 
 
-// Scenario('Aba "Já Sou cliente" ', () => {
+Scenario('Aba "Já Sou cliente" ', () => {
 
 
-//   //EU VOU PARA PAGINA "JÁ SOU CLIENTE"
-//   I.click('/html/body/my-app/login-component/div/div[1]/div/form/div[2]/button[1]')
-
-//   //EU PREENCHO OS CAMPOS COM OS DADOS
-//   I.fillField('Razão Social', Razao_Social)
-//   I.fillField('CNPJ', CNPJ_CORRETO)
-//   I.fillField('Telefone', Telefone)
-//   I.fillField('E-mail', Email)
+  //EU VOU PARA PAGINA "JÁ SOU CLIENTE"
+  I.click('/html/body/my-app/login-component/div/div[1]/div/form/div[2]/button[1]')
+
+  //EU PREENCHO OS CAMPOS COM OS DADOS
+  I.fillField('Razão Social', Razao_Social)
+  I.fillField('CNPJ', CNPJ_CORRETO)
+  I.fillField('Telefone', Telefone)
+  I.fillField('E-mail', Email)
 
-//   //EU APERTO O reCAPTCH PRA ENVIAR O FORMULARIO.
-//   // 
-//   //
-//   //
+  //EU APERTO O reCAPTCH PRA ENVIAR O FORMULARIO.
+  // 
+  //
+  //
 
-// }).tag('@JaSouCliente')
+}).tag('@JaSouCliente')
 
 
-// Scenario('Aba "Quero ser cliente" ', () => {
+Scenario('Aba "Quero ser cliente" ', () => {
 
 
-//   //VOU PARA ABA "QUERO SER CLIENTE"
-//   I.click('Quero ser cliente')
+  //VOU PARA ABA "QUERO SER CLIENTE"
+  I.click('Quero ser cliente')
 
-//   //EU PREENCHO OS CAMPOS COM OS DADOS
+  //EU PREENCHO OS CAMPOS COM OS DADOS
 
-//   //ADICIONO A RAZAO SOCIAL
-//   I.fillField('Razão Social', Razao_Social)
-//   //ADICIONO O CNPJ
-//   I.fillField('CNPJ', '0000000000000')
-//   //ADICIONO O TELEFONE
-//   I.fillField('Telefone', Telefone)
-//   //ADICIONO A INSCRIÇÃO ESTADUAL 
-//   I.fillField('Inscrição Estadual', '120000000')
-//   //ADICIONO O E-MAIL NFe
-//   I.fillField('E-mail NFe', 'Email.nfeTeste@gmail.com')
-//   //ADICIONO AO NOME FANTASIA 
-//   I.fillField('Nome Fantasia', Nome_Fantasia)
+  //ADICIONO A RAZAO SOCIAL
+  I.fillField('Razão Social', Razao_Social)
+  //ADICIONO O CNPJ
+  I.fillField('CNPJ', '0000000000000')
+  //ADICIONO O TELEFONE
+  I.fillField('Telefone', Telefone)
+  //ADICIONO A INSCRIÇÃO ESTADUAL 
+  I.fillField('Inscrição Estadual', '120000000')
+  //ADICIONO O E-MAIL NFe
+  I.fillField('E-mail NFe', 'Email.nfeTeste@gmail.com')
+  //ADICIONO AO NOME FANTASIA 
+  I.fillField('Nome Fantasia', Nome_Fantasia)
 
-//   // //EU SELECIONO O ESTADO DO AMAZONAS
-//   // I.click('UF')
-//   // I.click('/html/body/div[1]/div[3]/div/div/mat-option[2]')
+  // //EU SELECIONO O ESTADO DO AMAZONAS
+  // I.click('UF')
+  // I.click('/html/body/div[1]/div[3]/div/div/mat-option[2]')
 
-//   //DADOS DO FINANCEIRO
+  //DADOS DO FINANCEIRO
 
-//   //ADICIONO O EMAIL DO FINANCEIRO
-//   I.fillField(Email_Financeiro, Email)
-//   //ADICIONO O NOME DO CONTADO DO FINANCEIRO
-//   I.fillField(Nome_Contato_Financeiro, 'NomeFinanceiro')
-//   //ADICIONO AO NUMERO DE TELEFONE DO FINANCEIRO
-//   I.fillField(Numero_Financeiro, '48900000000')
-//   //CLICO NO BOTAO "AUTORIZADO A COMPRAR"
-//   I.click('Autorizado a comprar')
+  //ADICIONO O EMAIL DO FINANCEIRO
+  I.fillField(Email_Financeiro, Email)
+  //ADICIONO O NOME DO CONTADO DO FINANCEIRO
+  I.fillField(Nome_Contato_Financeiro, 'NomeFinanceiro')
+  //ADICIONO AO NUMERO DE TELEFONE DO FINANCEIRO
+  I.fillField(Numero_Financeiro, '48900000000')
+  //CLICO NO BOTAO "AUTORIZADO A COMPRAR"
+  I.click('Autorizado a comprar')
 
-//   //DADOS DO COMPRAS
+  //DADOS DO COMPRAS
 
-//   //ADICIONO O EMAIL DO COMPRAR
-//   I.fillField(Email_Compras, Email)
-//   //ADICIONO O NOME DO CONTADO DO COMPRAS
-//   I.fillField(Nome_Contato_Compras, 'NomeCompras')
-//   //ADICIONO AO NUMERO DE TELEFONE DO COMPRAR
-//   I.fillField(Numero_compras, '48900000000')
+  //ADICIONO O EMAIL DO COMPRAR
+  I.fillField(Email_Compras, Email)
+  //ADICIONO O NOME DO CONTADO DO COMPRAS
+  I.fillField(Nome_Contato_Compras, 'NomeCompras')
+  //ADICIONO AO NUMERO DE TELEFONE DO COMPRAR
+  I.fillField(Numero_compras, '48900000000')
 
-//   //DOU UM SCROL PARA BAIXO 
-//   I.scrollPageToBottom()
+  //DOU UM SCROL PARA BAIXO 
+  I.scrollPageToBottom()
 
-//   //COLOCO ADICIONO OS FORNECEDORES ATUAIS
-//   I.fillField('/html/body/my-app/cadastro-novo-cliente-component/div/div/div/form/div[1]/div[4]/div[8]/div[2]/input', 'FORNECEDOR TESTE ')
-//   I.fillField('/html/body/my-app/cadastro-novo-cliente-component/div/div/div/form/div[1]/div[4]/div[8]/div[3]/input', ' FORNECEDOR TESTE ')
-//   I.fillField('/html/body/my-app/cadastro-novo-cliente-component/div/div/div/form/div[1]/div[4]/div[8]/div[4]/input', 'FORNECEDOR TESTE ')
+  //COLOCO ADICIONO OS FORNECEDORES ATUAIS
+  I.fillField('/html/body/my-app/cadastro-novo-cliente-component/div/div/div/form/div[1]/div[4]/div[8]/div[2]/input', 'FORNECEDOR TESTE ')
+  I.fillField('/html/body/my-app/cadastro-novo-cliente-component/div/div/div/form/div[1]/div[4]/div[8]/div[3]/input', ' FORNECEDOR TESTE ')
+  I.fillField('/html/body/my-app/cadastro-novo-cliente-component/div/div/div/form/div[1]/div[4]/div[8]/div[4]/input', 'FORNECEDOR TESTE ')
 
-//   //ADICIONO NA AREA TEXTO OS ITENS QUE EU QUERO.
-//   I.fillField('/html/body/my-app/cadastro-novo-cliente-component/div/div/div/form/div[1]/div[4]/div[10]/mat-form-field/div/div[1]/div/textarea', 'Maquinas de testes')
+  //ADICIONO NA AREA TEXTO OS ITENS QUE EU QUERO.
+  I.fillField('/html/body/my-app/cadastro-novo-cliente-component/div/div/div/form/div[1]/div[4]/div[10]/mat-form-field/div/div[1]/div/textarea', 'Maquinas de testes')
 
-//   //ADICIONO QUAL O CANAL DE ATENDIMENTO 
-//   I.click('Quero comprar presencialmente pelo representante comercial')
-//   I.click(' Quero comprar pelo televendas via WhatsApp, e-mail e telefone')
-//   I.click('Quero comprar online pelo site ou aplicativo')
+  //ADICIONO QUAL O CANAL DE ATENDIMENTO 
+  I.click('Quero comprar presencialmente pelo representante comercial')
+  I.click(' Quero comprar pelo televendas via WhatsApp, e-mail e telefone')
+  I.click('Quero comprar online pelo site ou aplicativo')
 
-//   //DOU UM SCROL PARA BAIXO 
-//   I.scrollPageToBottom()
+  //DOU UM SCROL PARA BAIXO 
+  I.scrollPageToBottom()
 
-//   //SELECIONO QUAL REDE SOCIAL EU CONHECI A REZZADORI
-//   I.click('Instagram')
-//   I.click('Site')
-//   I.click('Visita')
-//   I.click('Google')
+  //SELECIONO QUAL REDE SOCIAL EU CONHECI A REZZADORI
+  I.click('Instagram')
+  I.click('Site')
+  I.click('Visita')
+  I.click('Google')
 
-//   I.click('Indicação')
-//   I.waitForText('Digite a indicação de como nos conheceu*')
-//   I.fillField('/html/body/my-app/cadastro-novo-cliente-component/div/div/div/form/div[1]/div[7]/div[2]/mat-radio-group/div[5]/input', 'TESTE PELO GOOGLE')
+  I.click('Indicação')
+  I.waitForText('Digite a indicação de como nos conheceu*')
+  I.fillField('/html/body/my-app/cadastro-novo-cliente-component/div/div/div/form/div[1]/div[7]/div[2]/mat-radio-group/div[5]/input', 'TESTE PELO GOOGLE')
 
-//   I.click('Outro')
-//   I.waitForText('Digite o outro meio de como nos conheceu*')
-//   I.fillField('/html/body/my-app/cadastro-novo-cliente-component/div/div/div/form/div[1]/div[7]/div[2]/mat-radio-group/div[6]/input', 'TESTE PELO GOOGLE')
+  I.click('Outro')
+  I.waitForText('Digite o outro meio de como nos conheceu*')
+  I.fillField('/html/body/my-app/cadastro-novo-cliente-component/div/div/div/form/div[1]/div[7]/div[2]/mat-radio-group/div[6]/input', 'TESTE PELO GOOGLE')
 
-//   I.saveScreenshot('@QueroSerCliente.png')
+  I.saveScreenshot('@QueroSerCliente.png')
 
-//   //AQUI PRECISA VALIDAR O reCAPTCHA PRA PODER ENVIAR O FORMULARIO.
-//   //
-//   //
-//   //
-//   // 
+  //AQUI PRECISA VALIDAR O reCAPTCHA PRA PODER ENVIAR O FORMULARIO.
+  //
+  //
+  //
+  // 
 
-// }).tag('@QueroSerCliente')
+}).tag('@QueroSerCliente')
 
 
-// Scenario('Aba "Esqueci a senha" ', () => {
+Scenario('Aba "Esqueci a senha" ', () => {
 
 
-//   //VOU PARA ABA "ESQUECI MINHA SENHA "
-//   I.click('Esqueci minha senha')
-//   I.fillField('CNPJ', CNPJ_CORRETO)
-//   I.fillField('E-mail', Email)
+  //VOU PARA ABA "ESQUECI MINHA SENHA "
+  I.click('Esqueci minha senha')
+  I.fillField('CNPJ', CNPJ_CORRETO)
+  I.fillField('E-mail', Email)
 
 
 
-//   // PRECISA VALIDAR O reCAPTCHA AQUI
-//   //
-//   //
-//   //
+  // PRECISA VALIDAR O reCAPTCHA AQUI
+  //
+  //
+  //
 
-// }).tag('@EsqueciMinhaSenha')
+}).tag('@EsqueciMinhaSenha')
 
 
 
-// Scenario('Login com CNPJ Inválido', () => {
+Scenario('Login com CNPJ Inválido', () => {
 
-//   //FAÇO LOGIN COM CNPJ INVÁLIDO.
-//   Login(CNPJ_INVALIDO, SENHA_CORRETA)
-//   I.waitForText('Cliente não cadastrado.')
+  //FAÇO LOGIN COM CNPJ INVÁLIDO.
+  Login(CNPJ_INVALIDO, SENHA_CORRETA)
+  I.waitForText('Cliente não cadastrado.')
 
-//   //SALVO UMA PRINTSCREEN APENAS PARA IR PRO OUTPUT, CASO EU QUEIRA VERIFICAR O PASSO A PASSO
-//   I.saveScreenshot('@LoginCNPJinvalido.png')
+  //SALVO UMA PRINTSCREEN APENAS PARA IR PRO OUTPUT, CASO EU QUEIRA VERIFICAR O PASSO A PASSO
+  I.saveScreenshot('@LoginCNPJinvalido.png')
 
-// }).tag('@LoginCNPJinvalido')
+}).tag('@LoginCNPJinvalido')
 
 
 
-// Scenario('Login com Senha Inválida', () => {
+Scenario('Login com Senha Inválida', () => {
 
-//   //FAÇO LOGIN COM A SENHA INVALIDA.
-//   Login(CNPJ_CORRETO, SENHA_INVALIDA)
-//   I.waitForText('Senha inválida.')
+  //FAÇO LOGIN COM A SENHA INVALIDA.
+  Login(CNPJ_CORRETO, SENHA_INVALIDA)
+  I.waitForText('Senha inválida.')
 
-//   I.saveScreenshot('@LoginSenhaInvalida.png')
+  I.saveScreenshot('@LoginSenhaInvalida.png')
 
-// }).tag('@LoginSenhaInvalida')
+}).tag('@LoginSenhaInvalida')
 
 
 
-// Scenario('Login Correto', () => {
+Scenario('Login Correto', () => {
 
-//   //FAÇO LOGIN COM OS DADOS CORRETOS.
-//   Login(CNPJ_CORRETO, SENHA_CORRETA)
-//   I.waitForText('08070', 10)
+  //FAÇO LOGIN COM OS DADOS CORRETOS.
+  Login(CNPJ_CORRETO, SENHA_CORRETA)
+  I.waitForText('08070', 10)
 
-//   I.saveScreenshot('@LoginCorreto.png')
+  I.saveScreenshot('@LoginCorreto.png')
 
-// }).tag('@LoginCorreto')
+}).tag('@LoginCorreto')
 
-// Scenario('Logout', () => {
+Scenario('Logout', () => {
 
-//   //FAÇO LOGIN COM OS DADOS CORRETOS.
-//   Login(CNPJ_CORRETO, SENHA_CORRETA)
+  //FAÇO LOGIN COM OS DADOS CORRETOS.
+  Login(CNPJ_CORRETO, SENHA_CORRETA)
 
-//   //ESPERO PELA ABA "PRODUTOS"
-//   I.waitForText('08070', 10)
+  //ESPERO PELA ABA "PRODUTOS"
+  I.waitForText('08070', 10)
 
-//   //CLICO NO BOTAO DE MENU 
-//   I.click('/html/body/my-app/home-component/mat-toolbar/mat-toolbar-row[1]/button')
+  //CLICO NO BOTAO DE MENU 
+  I.click('/html/body/my-app/home-component/mat-toolbar/mat-toolbar-row[1]/button')
 
-//   //CLICO NO BOTAO SAIR
-//   I.waitForText('SAIR', 10)
-//   I.click('/html/body/my-app/home-component/mat-sidenav-container/mat-sidenav/mat-nav-list/a[7]/div')
+  //CLICO NO BOTAO SAIR
+  I.waitForText('SAIR', 10)
+  I.click('/html/body/my-app/home-component/mat-sidenav-container/mat-sidenav/mat-nav-list/a[7]/div')
 
-//   //AGUARDO PELO INPUT "CNPJ" NA ABA LOGIN
-//   I.waitForText('CNPJ', 5)
+  //AGUARDO PELO INPUT "CNPJ" NA ABA LOGIN
+  I.waitForText('CNPJ', 5)
 
-//   I.saveScreenshot('@Logout.png')
+  I.saveScreenshot('@Logout.png')
 
-// }).tag('@Logout')
+}).tag('@Logout')
--- steps/07-Senha_test.js
+++ steps/07-Senha_test.js
@@ -1,52 +1,52 @@
-// Feature('Senhas').tag('Senhas');
+Feature('Senhas').tag('Senhas');
 
 
-// const { Login } = require("../pages/login_page");
-// const { I } = inject()
+const { Login } = require("../pages/login_page");
+const { I } = inject()
 
-// //DADOS DE ACORDO COM O QUE PRECISO NA ABA INICIAL 
+//DADOS DE ACORDO COM O QUE PRECISO NA ABA INICIAL 
 
-// const CNPJ_CORRETO = '00409260000115'
-// const SENHA_CORRETA = '48303523'
-// const SENHA_ALTERADA = 'Flex48303523'
-
-
-// Before(() => {
+const CNPJ_CORRETO = '00409260000115'
+const SENHA_CORRETA = '48303523'
+const SENHA_ALTERADA = 'Flex48303523'
 
 
-//     // CASO ESTEJA FECHADO EU FAÇO O LOGIN
-//     tryTo(() => Login(CNPJ_CORRETO, SENHA_CORRETA))
-//     I.waitForText('08070', 10)
-
-// })
+Before(() => {
 
 
-// Scenario('Trocar Senha', () => {
+    // CASO ESTEJA FECHADO EU FAÇO O LOGIN
+    tryTo(() => Login(CNPJ_CORRETO, SENHA_CORRETA))
+    I.waitForText('08070', 10)
 
-//     //CLICO PRA ABRIR O MENU PARA ALTERAR A SENHA
-//     I.click('/html/body/my-app/home-component/mat-toolbar/mat-toolbar-row[1]/button')
-//     I.click('ALTERAR')
-//     I.fillField('Senha Antiga','48303523')
-//     I.fillField('Senha Nova','Flex48303523')
-//     I.fillField('Confirme a nova senha','Flex48303523')
-//     I.click('ALTERAR')
+})
 
-//     //VALIDAÇÃO
-//     I.waitForText('Senha alterada com sucesso',5)
 
-//     Login(CNPJ_CORRETO, SENHA_ALTERADA);
-//     I.waitForText('08070', 10)
+Scenario('Trocar Senha', () => {
 
-//     //CLICO PRA ABRIR O MENU PARA ALTERA A SENHA PARA A ANTERIOR 
-//     I.click('/html/body/my-app/home-component/mat-toolbar/mat-toolbar-row[1]/button')
-//     I.click('ALTERAR')
-//     I.fillField('Senha Antiga','Flex48303523')
-//     I.fillField('Senha Nova','48303523')
-//     I.fillField('Confirme a nova senha','48303523')
-//     I.click('ALTERAR')
+    //CLICO PRA ABRIR O MENU PARA ALTERAR A SENHA
+    I.click('/html/body/my-app/home-component/mat-toolbar/mat-toolbar-row[1]/button')
+    I.click('ALTERAR')
+    I.fillField('Senha Antiga','48303523')
+    I.fillField('Senha Nova','Flex48303523')
+    I.fillField('Confirme a nova senha','Flex48303523')
+    I.click('ALTERAR')
+
+    //VALIDAÇÃO
+    I.waitForText('Senha alterada com sucesso',5)
+
+    Login(CNPJ_CORRETO, SENHA_ALTERADA);
+    I.waitForText('08070', 10)
+
+    //CLICO PRA ABRIR O MENU PARA ALTERA A SENHA PARA A ANTERIOR 
+    I.click('/html/body/my-app/home-component/mat-toolbar/mat-toolbar-row[1]/button')
+    I.click('ALTERAR')
+    I.fillField('Senha Antiga','Flex48303523')
+    I.fillField('Senha Nova','48303523')
+    I.fillField('Confirme a nova senha','48303523')
+    I.click('ALTERAR')
     
-//     //VALIDAÇÃO
-//     I.waitForText('Senha alterada com sucesso',5)
+    //VALIDAÇÃO
+    I.waitForText('Senha alterada com sucesso',5)
 
 
-// }).tag('TrocarSenha')
+}).tag('TrocarSenha')
